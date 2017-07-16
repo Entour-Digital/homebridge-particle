021eb789b4ac06173dce8683a33e8e73446e1172
Matthieu Nelmes <matthieunelmes@gmail.com>
2017-07-16 22:27:05 +0100

    adding rbg states

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var request = require("request");
 var eventSource = require('eventsource');
 var Service, Characteristic;
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
-  
+
 	homebridge.registerPlatform("homebridge-particle", "Particle", ParticlePlatform);
 }
 
-function ParticlePlatform(log, config){
+function ParticlePlatform(log, config) {
 	this.log = log;
 	this.accessToken = config["access_token"];
 	this.deviceId = config["deviceid"];
@@ -18,21 +18,21 @@ function ParticlePlatform(log, config){
 }
 
 ParticlePlatform.prototype = {
-	accessories: function(callback){
+	accessories: function (callback) {
 		var foundAccessories = [];
-		
+
 		var count = this.devices.length;
-		
-		for(index=0; index< count; ++index){
-			var accessory  = new ParticleAccessory(
-				this.log, 
+
+		for (index = 0; index < count; ++index) {
+			var accessory = new ParticleAccessory(
+				this.log,
 				this.url,
 				this.accessToken,
 				this.devices[index]);
-			
+
 			foundAccessories.push(accessory);
 		}
-		
+
 		callback(foundAccessories);
 	}
 };
@@ -40,7 +40,7 @@ ParticlePlatform.prototype = {
 function ParticleAccessory(log, url, access_token, device) {
 	this.log = log;
 	this.name = device["name"],
-	this.args = device["args"];
+		this.args = device["args"];
 	this.deviceId = device["deviceid"];
 	this.type = device["type"];
 	this.functionName = device["function_name"];
@@ -50,94 +50,111 @@ function ParticleAccessory(log, url, access_token, device) {
 	this.accessToken = access_token;
 	this.url = url;
 	this.value = 20;
-	
+
 	console.log(this.name + " = " + this.sensorType);
-	
+
 	this.services = [];
-	
+
 	this.informationService = new Service.AccessoryInformation();
 
 	this.informationService
 		.setCharacteristic(Characteristic.Manufacturer, "Particle")
 		.setCharacteristic(Characteristic.Model, "Photon")
 		.setCharacteristic(Characteristic.SerialNumber, "AA098BB09");
-		
+
 	this.services.push(this.informationService);
-  
-	if(this.type === "LIGHT"){
+
+	if (this.type === "LIGHT") {
 		this.lightService = new Service.Lightbulb(this.name);
-		
+
 		this.lightService
 			.getCharacteristic(Characteristic.On)
 			.on('set', this.setState.bind(this));
-			
+
 		this.services.push(this.lightService);
 	}
-	else if(this.type === "SENSOR"){
+	if (this.type === "RGBLIGHT") {
+		this.lightService = new Service.Lightbulb(this.name);
+
+		this.lightService
+			.getCharacteristic(Characteristic.On)
+			.on('set', this.setState.bind(this));
+
+		this.lightService
+			.getCharacteristic(new Characteristic.Brightness())
+			.on('set', this.setBrightness.bind(this));
+
+		bulbService
+			.getCharacteristic(new Characteristic.Hue())
+			.on('set', this.setHue.bind(this));
+
+		this.services.push(this.lightService);
+	}
+	else if (this.type === "SENSOR") {
 		var service;
-		
+
 		console.log("Sensor Type: " + this.sensorType.toLowerCase());
 
-		if(this.sensorType.toLowerCase() === "temperature"){
+		if (this.sensorType.toLowerCase() === "temperature") {
 			console.log("Temperature Sensor");
-			
+
 			service = new Service.TemperatureSensor(this.name);
-			
+
 			service
 				.getCharacteristic(Characteristic.CurrentTemperature)
 				.on('get', this.getDefaultValue.bind(this));
 		}
-		else if(this.sensorType.toLowerCase() === "humidity"){
+		else if (this.sensorType.toLowerCase() === "humidity") {
 			console.log("Humidity Sensor");
-			
+
 			service = new Service.HumiditySensor(this.name);
-			
+
 			service
 				.getCharacteristic(Characteristic.CurrentRelativeHumidity)
 				.on('get', this.getDefaultValue.bind(this));
 		}
-		else if(this.sensorType.toLowerCase() === "light"){
+		else if (this.sensorType.toLowerCase() === "light") {
 			console.log("Light Sensor");
-			
+
 			service = new Service.LightSensor(this.name);
-			
+
 			service
 				.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
 				.on('get', this.getDefaultValue.bind(this));
 		}
-			
-		if(service != undefined){
+
+		if (service != undefined) {
 			console.log("Initializing " + service.displayName + ", " + this.sensorType);
-			
+
 			var eventUrl = this.url + this.deviceId + "/events/" + this.eventName + "?access_token=" + this.accessToken;
 			var es = new eventSource(eventUrl);
 
 			console.log(eventUrl);
 
-			es.onerror = function() {
+			es.onerror = function () {
 				console.log('ERROR!');
 			};
 
 			es.addEventListener(this.eventName,
 				this.processEventData.bind(this), false);
-			
+
 			this.services.push(service);
 		}
-		
+
 		console.log("Servie Count: " + this.services.length);
 	}
 }
 
-ParticleAccessory.prototype.setState = function(state, callback) {
+ParticleAccessory.prototype.setState = function (state, callback) {
 	this.log.info("Getting current state...");
-	
+
 	this.log.info("URL: " + this.url);
 	this.log.info("Device ID: " + this.deviceId);
-  
+
 	var onUrl = this.url + this.deviceId + "/" + this.functionName;
-	
+
 	this.log.info("Calling function: " + onUrl);
-	
+
 	var argument = this.args.replace("{STATE}", (state ? "1" : "0"));
 
 	request.post(
@@ -147,7 +164,7 @@ ParticleAccessory.prototype.setState = function(state, callback) {
 				args: argument
 			}
 		},
-		function(error, response, body) {
+		function (error, response, body) {
 			console.log(response);
 
 			if (!error) {
@@ -159,14 +176,22 @@ ParticleAccessory.prototype.setState = function(state, callback) {
 	);
 }
 
-ParticleAccessory.prototype.processEventData = function(e){
+Particle.prototype.setBrightness = function(state, callback){
+
+}
+
+Particle.prototype.setHue = function(state, callback){
+	
+}
+
+ParticleAccessory.prototype.processEventData = function (e) {
 	var data = JSON.parse(e.data);
 	var tokens = data.data.split('=');
-	
+
 	console.log(tokens[0] + " = " + tokens[1] + ", " + this.services[1].displayName + ", " + this.sensorType + ", " + this.key.toLowerCase() + ", " + tokens[0].toLowerCase());
 	console.log(this.services[1] != undefined && this.key.toLowerCase() === tokens[0].toLowerCase());
-	
-	if(this.services[1] != undefined && this.key.toLowerCase() === tokens[0].toLowerCase()){	
+
+	if (this.services[1] != undefined && this.key.toLowerCase() === tokens[0].toLowerCase()) {
 		if (tokens[0].toLowerCase() === "temperature") {
 			this.value = parseFloat(tokens[1]);
 
@@ -191,16 +216,16 @@ ParticleAccessory.prototype.processEventData = function(e){
 	}
 }
 
-ParticleAccessory.prototype.getDefaultValue = function(callback) {
+ParticleAccessory.prototype.getDefaultValue = function (callback) {
 	callback(null, this.value);
 }
 
-ParticleAccessory.prototype.setCurrentValue = function(value, callback) {
+ParticleAccessory.prototype.setCurrentValue = function (value, callback) {
 	console.log("Value: " + value);
 
 	callback(null, value);
 }
 
-ParticleAccessory.prototype.getServices = function() {
+ParticleAccessory.prototype.getServices = function () {
 	return this.services;
 }
